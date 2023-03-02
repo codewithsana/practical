028c986f628d3a3b337bc2ca26bfbfbdfa42eea9
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-03-02 18:35:30 +0530

    Add files via upload

---
+++ Array_eg4.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class Array_eg4 {//main class
+public static void main(String[] args) {//main method
+	//declare and initialized
+ String name[]= {"Priya","Taniya","Riya","Rahul","Jay","Yash","Sana","Bhumi","Tarun","pihu"};
+for(int i=0;i<name.length;i++) {//length  is predefine
+System.out.println(name[i]);	
+}
+}
+}
