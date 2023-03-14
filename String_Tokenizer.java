63378fa75a7d60d380d47cbbe8805e7c0958ab8a
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-03-14 18:12:13 +0530

    Add files via upload

---
+++ String_Tokenizer.java
@@ -0,0 +1,17 @@
+package String;
+import java.util.StringTokenizer;
+public class String_Tokenizer {
+public static void my() {
+	StringTokenizer t= new StringTokenizer("Anudip Foundation skill and carrer Development center");
+while(t.hasMoreTokens()) {//boolean method it checks if there is no more token available
+System.out.println(t.nextToken());//string method it return the next token from string token
+}
+StringTokenizer t1= new StringTokenizer("I am & sana & staying in Ghaziabad");
+while(t1.hasMoreTokens()) {
+	System.out.println(t1.nextToken("&"));//to break the string
+}
+}
+public static void main(String[] args) {//main method
+	my();//without object because its static type
+}
+}
