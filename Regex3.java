53ff21cb1e9790a58e918edd6c6131aeed6ffa21
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-03-16 17:25:01 +0530

    Add files via upload

--- Regex3.java
+++ Regex3.java
@@ -9,16 +9,23 @@ import java.util.regex.*;
 //x{n,} - x occur n or more time x(7) only 7 char or more
 public class Regex3 {
 public static void main(String[] args) {
-	boolean b1= Pattern.matches("[A-Za-z0-9]{12}","Anudip123ppp");
-System.out.println(b1);
-boolean b2= Pattern.matches("[A-Z a-z 0-9.@]{19,}","Pallabi.Anudip@2023");
-System.out.println(b2);
-boolean b3= Pattern.matches("[0-9]{10}","9878965413");
-System.out.println(b3);
-boolean b4= Pattern.matches("[6789]{1}[0-9]{9}","9878965413");
-System.out.println(b4);
-boolean b5= Pattern.matches("[A-Za-z0-9.@]{30,}","sana77saifi@gmail.com");
+//	boolean b1= Pattern.matches("[A-Za-z0-9]{12,}","Anudip123ppp");
+//System.out.println(b1);
+//boolean b2= Pattern.matches("[A-Z a-z 0-9.@]{19,}","Pallabi.Anudip@2023");
+//System.out.println(b2);
+//boolean b3= Pattern.matches("[0-9]{10}","9878965413");
+//System.out.println(b3);
+//boolean b4= Pattern.matches("[6789]{1}[0-9]{9}","9878965413");
+//System.out.println(b4);
+boolean b5= Pattern.matches("[A-Za-z0-9.@]{21}","sana77saifi@gmail.com");
 System.out.println(b5);
-
+boolean b6= Pattern.matches("[a-z0-9+_.-]*@[a-z]*+\\.[a-z]{3}","sana77saifi@gmail.com");
+System.out.println(b6);
+//META CHARACTER
+//   \d-[0-9]   
+//   \s- white space character-----blank space 
+//   \w-[a-zA-Z0-9]
+boolean b7= Pattern.matches("\\w{9}","Anudip123");
+System.out.println(b7);
 }
 }
