1d7c2db573e0c425f05b093a6f4a867fe87ecfd2
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-03-22 16:06:04 +0530

    Add files via upload

---
+++ Throweg1.java
@@ -0,0 +1,16 @@
+package Exception;
+// throw checked
+public class Throweg1 {
+public static void checknum(int num) {//static method
+	if(num<0) {
+		throw new ArithmeticException("number is negetive");
+	}
+	else {
+		System.out.println("number is positive");
+	}
+}
+public static void main(String[] args) {
+	checknum(-1);
+	System.out.println("executed");
+}
+}
