cb8b5ebe3c0e6a4aaadf3b4ff831a15b28dfd646
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-03-27 21:20:07 +0530

    Add files via upload

---
+++ ThreadEg2_WithoutExtend.java
@@ -0,0 +1,18 @@
+package Thread;
+//directly we use thread class without extend
+//with and without set name we can name the thread
+public class ThreadEg2_WithoutExtend{
+public void run() {
+	System.out.println("Sana");
+}
+public static void main(String[] args) {
+	//creating an object of the thread class using the thread constructor
+	Thread t= new Thread("Anudip");//thread 0
+	Thread t1= new Thread();
+	t.start();
+	t1.start();
+	t1.setName("Foundation");//thread 2
+	System.out.println("Thread 0: "+t.getName());
+	System.out.println("Thread 1: "+t1.getName());
+}
+}
