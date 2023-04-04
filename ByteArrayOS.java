7f4751e13f9ea138463f24a96d0a0a2863a03ab9
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-04-04 17:56:10 +0530

    Add files via upload

---
+++ ByteArrayOS.java
@@ -0,0 +1,25 @@
+package Input_Output;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+/*ByteArrayOutputStream class is used to write same
+ data info multiple files,the data is written into a
+  byte array,which can be written to multiple stream later */
+public class ByteArrayOS {
+public static void main(String[] args) throws IOException{
+	//can be written to multiple stream
+	FileOutputStream f= new FileOutputStream("E:\\java_io\\stringcode.txt");
+	FileOutputStream f1= new FileOutputStream("E:\\java_io\\btyecode.txt");
+	FileOutputStream f2= new FileOutputStream("E:\\java_io\\input.txt");
+	ByteArrayOutputStream bt= new ByteArrayOutputStream ();
+	//one message  send into multiple stream
+	bt.write(65);//ASCII value 65 is A
+	bt.writeTo(f);
+	bt.writeTo(f1);
+	bt.writeTo(f2);
+	bt.flush();
+	bt.close();
+	System.out.println("done");
+}
+}
